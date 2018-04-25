26dd38a9e206f200ee9b6b0f002a7ee1ba070423
Justin <nak1411@gmail.com>
2018-04-24 23:24:41 -0700

    Added more detailed comment

--- media.py
+++ media.py
@@ -2,18 +2,29 @@ import webbrowser
 
 
 class Movie():
-    """ This class contains movie related data such as ratings, title, story,
-    poster image and trailer URL."""
+    """ Contains movie related data such as title, story,
+    poster image URL and trailer URL.
+
+    Args:
+        title (str): Title of movie instance.
+        storyline (str): Description of storyline of movie instance.
+        poster_image_url (str): URL to the movie's poster image.
+        trailer_youtube_url (str): Youtube URL to the movie's trailer.
+
+    Attributes:
+        movie_title (str): Title of movie instance.
+        movie_storyline (str): Description of storyline of movie instance.
+        poster_image (str): URL to the movie's poster image.
+        trailer_youtube (str): Youtube URL to the movie's trailer.
+    """
 
     def __init__(self, movie_title, movie_storyline, poster_image,
                  trailer_youtube):
-        """Inits Movie class with title, story, poster image and trailer URL
-        strings."""
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
     def show_trailer(self):
-        """Opens Youtube URL and plays movie trailer."""
+        """ Opens browser to movie trailer URL."""
         webbrowser.open(self.trailer_youtube_url)
